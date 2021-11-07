31b6ef1bc2b79fa84886d956309038c2eed9dd45
Dylan <dylanrolk@gmail.com>
2021-11-07 15:14:51 -0500

    added zoom functionality

--- frontEnd/src/components/canvas/Canvas.js
+++ frontEnd/src/components/canvas/Canvas.js
@@ -5,81 +5,135 @@ class Canvas extends React.Component {
 	constructor(props){
 		super(props);
 		this.state={
-			ARR_NUM : 1, 
+			ARR_NUM : this.props.data[1].length-4, 
 			MOUSE_POS: 0,
 			SCALE_FACTOR: 1,
 		};
 		this.getMax=this.getMax.bind(this);
 		this.display=this.display.bind(this);
+		this.wheelZoom=this.wheelZoom.bind(this);
+		this.mouseMove=this.mouseMove.bind(this);
 	}
 
 
-	getMax(x){
+	getMax(x,arraynum,data){
 
 		var max=[x,0,0] 
 		for(var i=0; i<(this.state.SCALE_FACTOR); i++){
 			max[0]=x
-			if(this.props.data[this.state.ARR_NUM][x]>max[1])
-				max[1]=this.props.data[this.state.ARR_NUM][x]
+			if(data[arraynum][this.state.ARR_NUM][x]>max[1])
+				max[1]=data[arraynum][this.state.ARR_NUM][x]
 
-			else if(this.props.data[this.state.ARR_NUM][x]<max[2])
-				max[2]=this.props.data[this.state.ARR_NUM][x]
+			else if(data[arraynum][this.state.ARR_NUM][x]<max[2])
+				max[2]=data[arraynum][this.state.ARR_NUM][x]
 			x++
 		}
 		return max
 	}
+
+
 	display(){
-		console.log("hello!")
-		const canvas = this.refs.canvas
-		const c= canvas.getContext("2d")
-		console.log(c);
-		var x_coordinate=0;
-		var lines=0;
-		c.beginPath();
+		var canvas=this.refs.canvas;
+		var c=canvas.getContext('2d');
+		var data=this.props.data;
+		
+		var SCALE_FACTOR= this.state.SCALE_FACTOR
+		
+		c.canvas.width=1920
 		c.moveTo(0, 250)
 		c.lineTo(1920, 250)
 		c.stroke();
 		
-		//weird starting variable for zooming
-		for(var x = Math.floor(this.state.MOUSE_POS*(20-this.state.SCALE_FACTOR)/Math.pow(20, this.state.ARR_NUM)); x<this.props.data[this.state.ARR_NUM].length; x++){
-			console.log("WOOO!")
-			if(lines>=1920) break;
-			if(this.state.SCALE_FACTOR>1){
-				var y = this.getMax(x)
-				c.moveTo(x_coordinate*2,0-y[2]+250) 
-				c.lineTo(x_coordinate, 0-y[1]+250)
+		for(var i = 1; i<data.length; i++){
+			var x_coordinate=0;
+			c.beginPath();
+			var lines=0;
+			var max=0;
+			
+			for(var x = 0; x<data[i][this.state.ARR_NUM].length; x++){
+				
+				if(lines>=1920) break;
+
+				if(SCALE_FACTOR>1){
+					var y = this.getMax(x,i,data)
+						
+							c.moveTo(x_coordinate,(0-y[2])/data[0][0]+250) 
+							c.lineTo(x_coordinate, (0-y[1])/data[0][0]+250)
+				}
+				else if (SCALE_FACTOR<0) {
+					c.moveTo(x_coordinate*Math.abs(SCALE_FACTOR), 250) 
+					c.lineTo(x_coordinate*Math.abs(SCALE_FACTOR), (0-data[i][this.state.ARR_NUM][x]/data[0][0][0])+250)
+					var y=[x,0,0]
+				}
+				else{
+					c.moveTo(x_coordinate, 250) 
+					c.lineTo(x_coordinate, (0-data[i][this.state.ARR_NUM][x]/data[0][0][0])+250)
+					var y=[x,0,0]
+				}
+
+				lines++;
+				
+				x_coordinate+=1;
+				x=y[0]
+				max=x;
 			}
-			else{
-				c.moveTo(x_coordinate*5, 250) 
-				c.lineTo(x_coordinate*5, ((0-this.props.data[this.state.ARR_NUM][x])/this.props.data[0])+250)
-				y=[x,0,0]
-			}
-			lines++;
-			x_coordinate++;
-			x=y[0]
+			// console.log(max,"***");
+			var colors = ['red', 'green', 'blue']
+			c.strokeStyle=colors[i];
 			c.stroke();
-			
+			c.closePath();
 		}
-		c.closePath();
+		// console.log(max);
+		// console.log(lines);
 			
+	}
+	wheelZoom(event){
+		event.preventDefault();
+
+		this.state.SCALE_FACTOR= this.state.SCALE_FACTOR+=Math.floor(event.deltaY/102);
+		
+		
+	
+		if(this.state.SCALE_FACTOR > 19 && this.state.ARR_NUM<this.props.data.length){
+			this.state.SCALE_FACTOR=2;
+			this.state.ARR_NUM++;
+		}
+		else if(this.state.SCALE_FACTOR <= 1 && this.state.ARR_NUM>=1){
+			this.state.SCALE_FACTOR=19;
+			this.state.ARR_NUM--;
+		}
+		console.log(this.state.SCALE_FACTOR, this.state.ARR_NUM);
+		this.display();
+	}
+	mouseMove(event){
+		var canvas=this.refs.canvas;
+		var cRect = canvas.getBoundingClientRect();
+		this.state.MOUSE_POS = Math.round(event.clientX - cRect.left);
 	}
 	componentDidMount(){
 		const canvas= this.refs.canvas;
 		const c= canvas.getContext("2d")
+		c.clearRect(0, 0, canvas.width, canvas.height);
+		canvas.addEventListener("wheel", this.wheelZoom);
+		canvas.addEventListener("mousemove", this.mouseMove);
+		// this.display();
 		
-		// c.moveTo(0, 250)
-		// c.lineTo(1920, 250)
-		// c.stroke()
+		c.moveTo(0, 250)
+		c.lineTo(1920, 250)
+		c.stroke()
 		
 	}
    componentDidUpdate() {
+	   console.log("update");
 	   const canvas= this.refs.canvas;
 		const c= canvas.getContext("2d")
+		c.clearRect(0, 0, canvas.width, canvas.height);
 		if(this.props.data.length > 0)
 			this.display();
 		
 	}
   render(){
+	  console.log("rendered");
 	const styles={
 		border: "1px solid black"
 		
--- nyquist-test/app.py
+++ nyquist-test/app.py
@@ -37,11 +37,35 @@ def process():
 		print(len(sound))
 		wave_file=wave.open(request.files['file'], mode='rb')
 		
+		if sound.ndim > 1 :
+			sound= sound[1:, 0]
 		
+	def compress(returnarray, scale):
+		temparray=[]
+		for i in range(0,len(returnarray)-scale,scale):
+			min=0;
+			max=0;
+			for j in range(scale):
+				if returnarray[i+j] > max:
+					max = returnarray[i+j]
+				elif returnarray[i+j] < min:
+					min = returnarray[i+j]
+			temparray.append(max)
+			temparray.append(min)
+		return temparray;
+		
+	sound2=[(sound/2).tolist()]
+	sound=[sound.tolist()]
+	
+	
+	while len(sound[len(sound)-1])> 2:
+		print("GOOD");
+		sound.append(compress(sound[len(sound)-1],20))
+	
+	while len(sound2[len(sound2)-1])> 2:
+		sound2.append(compress(sound2[len(sound2)-1],20))
 		
 
-
-		
 
 		
 		# bytes_wav = bytes()
@@ -60,7 +84,7 @@ def process():
 		# wav_bytes = byte_io.read()
 		# audio_data = base64.b64encode(wav_bytes).decode('UTF-8')
 
-	return str([[pow(8,wave_file.getsampwidth())],sound.tolist()])
+	return str([[[pow(8,wave_file.getsampwidth())]], sound, sound2])
 	# else:
 		# print('false')
 

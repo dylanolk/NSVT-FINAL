a6bf91f9e841c1e0132e1c47e8c5874282497344
eric <eric@eric-air.local>
2021-11-06 20:05:05 -0400

    proper backend

--- nyquist-test/app.py
+++ nyquist-test/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, url_for, redirect
 from scipy.io import wavfile
-from thm import freq
+from thm import *
 from io import StringIO, BytesIO
 import base64
 
@@ -17,13 +17,24 @@ def process():
 	print(request.files)
 	if 'file' in request.files:
 		print('true')
-		sampFreq, sound = wavfile.read(request.files['file'])
+		try:
+			sampFreq, sound = wavfile.read(request.files['file'])
+		except:
+			return redirect("/")
+
 		print(sampFreq, sound)
 		print(len(sound))
 
 		bytes_wav = bytes()
 		byte_io = BytesIO(bytes_wav)
-		wavfile.write(byte_io, sampFreq, sound)
+
+		#wavfile.write(byte_io, sampFreq, sound)
+
+		newsound = resample_(sound)
+		print(newsound[1])
+
+		wavfile.write(byte_io, newsound[1], newsound[0])
+
 		wav_bytes = byte_io.read()
 		audio_data = base64.b64encode(wav_bytes).decode('UTF-8')
 

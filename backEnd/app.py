5922238998d7da6bc1366bb644ab78d0e22d3855
eric <eric@eric-air.local>
2021-12-15 15:01:14 -0500

    comments

--- backEnd/app.py
+++ backEnd/app.py
@@ -5,13 +5,16 @@ from io import StringIO, BytesIO
 import base64
 import json
 from flask_cors import CORS
-import wave
 
 
 app = Flask(__name__)
-CORS(app)
 
 
+CORS(app) # remove for live site
+
+'''
+Compresses the given array
+'''
 def compress(returnarray, scale):
     temparray = []
     for i in range(0, len(returnarray)-scale, scale):
@@ -26,45 +29,64 @@ def compress(returnarray, scale):
         temparray.append(min)
     return temparray
 
-
+'''
+Default route to serve the web page
+'''
 @app.route("/")
 def index():
     return render_template("home.html")
 
 
+'''
+Anonymous fxn inside process to facilitate creation of audio blobs
+INPUT: array (floats), frequency (int)
+OUTPUT: base64 representation
+'''
+
+def create_aud_buf(audio, freq):
+    bytes_wav = bytes()
+    byte_io = BytesIO(bytes_wav)
+    wavfile.write(byte_io, freq, audio)
+    wav_bytes = byte_io.read()
+
+    audio_data = base64.b64encode(wav_bytes)
+    return audio_data
+
+'''
+Does most of the heavy lifting
+INPUT: Requires a POST file
+OUTPUT: Downsampled file
+'''
 @app.route("/process", methods=['POST', 'GET'])
 def process():
-    print("here")
+
+    #print("processing hit")
+
     print(request.files)
     if 'file' in request.files:
-        print('true')
+        #print('file exist')
         try:
-            sampFreq, sound = wavfile.read(request.files['file'])
+            samp_freq, sound = wavfile.read(request.files['file'])
+
+            if len(sound) > (1*(10**6)):
+                # Conforms the sound into a manageable size 
+                sound = sound[:100000]
+
         except:
             return redirect("/")
 
-        print(sampFreq, sound)
-        print(len(sound))
+        # print(samp_freq, sound)
+        # print(len(sound))
 
-        def create_aud_buf(audio, freq):
-            bytes_wav = bytes()
-            byte_io = BytesIO(bytes_wav)
-            wavfile.write(byte_io, freq, audio)
-            wav_bytes = byte_io.read()
+        # Audio buffer of the original
+        audio_data = create_aud_buf(sound, samp_freq)
 
-            audio_data = base64.b64encode(wav_bytes)
-            return audio_data
-
-        audio_data = create_aud_buf(sound, sampFreq)
-
+        # Checks the channels
         if sound.ndim > 1:
             sound = sound[1:, 0]
-            print("test1")
-        sound = sound[1:100000]
-
-
-
-        audio_out = resample_summary(sound, sampFreq)
+          
+        # Bulk of the output with various dict outputs 
+        audio_out = resample_summary(sound, samp_freq)
         print(audio_out)
 
         if len(audio_out) == 1:
@@ -72,21 +94,19 @@ def process():
         else:
             newsound = sound
 
-        change = sampFreq/audio_out[0]["ny_rate"]
 
+        # Get the rate of change from the original
+        change = samp_freq/audio_out[0]["ny_rate"]
+
+        # Audio buffer of modified
         audio_data_out = create_aud_buf(newsound, audio_out[0]["ny_rate"])
 
 
+        # Prepare the file
         wave_file = wave.open(request.files['file'], mode='rb')
         sound = [sound.tolist()]
         newsound = [newsound.tolist()]
 
-        print("===")
-        print(type(sound))
-        print(type(newsound))
-        print("===")
-        print(len(newsound[0]))
-        print(len(sound[0]))
 
         while len(sound[len(sound)-1]) > 2:
             print("GOOD")
@@ -105,30 +125,24 @@ def process():
         audio_transf = str(audio_data)
 
 
-        # return render_template("home.html", value = audio_transf, vlen = len(audio_transf) -1 )
         return response
 
     else:
         print('false')
 
-    #return render_template("home.html")
+    return {
+        "message": "Failure"
+    }
+
+
 
 def process_defaults(filepath):
 
-        sampFreq, sound = wavfile.read(filepath)
-        print(sampFreq, sound)
-        print(len(sound))
+        samp_freq, sound = wavfile.read(filepath)
+        # print(samp_freq, sound)
+        # print(len(sound))
 
-        def create_aud_buf(audio, freq):
-            bytes_wav = bytes()
-            byte_io = BytesIO(bytes_wav)
-            wavfile.write(byte_io, freq, audio)
-            wav_bytes = byte_io.read()
-
-            audio_data = base64.b64encode(wav_bytes)
-            return audio_data
-
-        audio_data = create_aud_buf(sound, sampFreq)
+        audio_data = create_aud_buf(sound, samp_freq)
 
         if sound.ndim > 1:
             sound = sound[1:, 0]
@@ -137,7 +151,7 @@ def process_defaults(filepath):
 
 
 
-        audio_out = resample_summary(sound, sampFreq)
+        audio_out = resample_summary(sound, samp_freq)
 
 
         if len(audio_out) == 1:
@@ -145,7 +159,7 @@ def process_defaults(filepath):
         else:
             newsound = sound
 
-        change = sampFreq/audio_out[0]["ny_rate"]
+        change = samp_freq/audio_out[0]["ny_rate"]
 
         audio_data_out = create_aud_buf(newsound, audio_out[0]["ny_rate"])
 
@@ -153,13 +167,6 @@ def process_defaults(filepath):
         wave_file = wave.open(filepath, mode='rb')
         sound = [sound.tolist()]
         newsound = [newsound.tolist()]
-
-        print("===")
-        print(type(sound))
-        print(type(newsound))
-        print("===")
-        print(len(newsound[0]))
-        print(len(sound[0]))
 
         while len(sound[len(sound)-1]) > 2:
             print("GOOD")
@@ -171,20 +178,15 @@ def process_defaults(filepath):
         data = str([[[pow(8, wave_file.getsampwidth())]],
                     sound, newsound, change])
 
-    
         return {"audio_in": str(audio_data), "data": data, "audio_out": str(audio_data_out)}
 
-     
-
-
-        # return render_template("home.html", value = audio_transf, vlen = len(audio_transf) -1 )
         
 
-	
+    
 @app.route("/defaults" )
 def defaults():
-	return {"cantina": process_defaults("./cantina.wav"), "sine": process_defaults("./sine.wav"), "star_wars" : process_defaults("./StarWars3.wav"), "taunt": process_defaults("taunt.wav")}
-	
+    return {"cantina": process_defaults("./cantina.wav"), "sine": process_defaults("./sine.wav"), "star_wars" : process_defaults("./StarWars3.wav"), "taunt": process_defaults("taunt.wav")}
+    
 
 if __name__ == '__main__':
     main()
